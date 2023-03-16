940a729489f0b3221da3c599058d3ef7a480b452
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-16 20:11:22 +0700

    edit login view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,83 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    .divider:after,
+    .divider:before {
+    content: "";
+    flex: 1;
+    height: 1px;
+    background: #eee;
+    }
+</style>
+<section class="vh-100">
+    <div class="container py-5 h-100">
+      <div class="row d-flex align-items-center justify-content-center h-100">
+        <div class="col-md-8 col-lg-7 col-xl-6">
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+            class="img-fluid" alt="Phone image">
+        </div>
+        <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+            <div class="card-header text-center">{{ __('Login') }}</div>
+            <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <!-- Email input -->
+            <div class="form-outline mb-4">
+                <input id="email" class="form-control form-control-lg @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus type="email" />
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              <label class="form-label" for="form1Example13">{{ __('Email Address') }}</label>
+            </div>
+  
+            <!-- Password input -->
+            <div class="form-outline mb-4">
+                <input id="password" type="password" class="form-control form-control-lg @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"  />
+              
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+              <label class="form-label" for="form1Example23">Password</label>
+            </div>
+  
+            <div class="d-flex justify-content-around align-items-center mb-4">
+                <!-- Checkbox -->
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                    <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
+                </div>
+                @if (Route::has('password.request'))
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Forgot Your Password?') }}
+                    </a>
+                @endif
+            </div>
+  
+            <!-- Submit button -->
+         
+          
+            <button type="submit" class="btn btn-primary">
+                {{ __('Login') }}
+            </button>
+
+                    
+               
+  
+          
+  
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
 <div class="container">
+{{--     
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -68,6 +144,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
