e1baa3bef9501f15ba598339e9f1d500037a7f96
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-17 21:01:36 +0700

    add controller chat function index

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    //
+    public function index(Request $request){
+        if ($request->user()->role == 'admin') {
+            // tampilkan dashboard admin
+            return view('dashboard.admin.chat.home');
+        } elseif ($request->user()->role == 'guru') {
+            // tampilkan dashboard guru
+            return view('dashboard.guru.chat.home');
+        } else {
+            // tampilkan halaman umum jika user tidak memiliki role yang sesuai
+            return view('dashboard.siswa.chat.home');
+        }
+    }
+}
