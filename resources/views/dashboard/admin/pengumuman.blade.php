41eb63c62fdf7b6c9299179efd33befdbf7257ea
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-19 21:11:08 +0700

    add view pengumuman admin

---
+++ resources/views/dashboard/admin/pengumuman.blade.php
@@ -0,0 +1,12 @@
+@extends('dashboard.admin.master')
+ 
+
+@section('konten')
+
+<div class="container-fluid">
+    <div class="col-8 " style="background-color: ">
+
+    </div>
+</div>
+ 
+@endsection
