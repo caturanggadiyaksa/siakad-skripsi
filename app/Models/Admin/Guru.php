8dc1b89aacf30c436f09cff40817707982fd71be
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-22 13:54:51 +0700

    add model guru untuk menu admin

---
+++ app/Models/Admin/Guru.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guru extends Model
+{
+    use HasFactory;
+    protected $table = "table_guru";
+}
