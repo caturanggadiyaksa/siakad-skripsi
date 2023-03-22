2ec07b8cc742ed6a5859c0c78ce7e2ed1fd683c7
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-22 13:53:11 +0700

    add function index di akse controller

--- app/Http/Controllers/admin/AksesController.php
+++ app/Http/Controllers/admin/AksesController.php
@@ -8,4 +8,21 @@ use Illuminate\Http\Request;
 class AksesController extends Controller
 {
     //
+    public function index(Request $request){
+
+        $data = ['title' => 'Manajemen Akses'];
+        
+        if ($request->user()->role == 'admin') {
+            // tampilkan dashboard admin
+            return view('dashboard.admin.akses', $data);
+        } else {
+            // tampilkan halaman umum jika user tidak memiliki role yang sesuai
+            return redirect('/login');
+            
+
+        }
+
+      
+
+    }
 }
