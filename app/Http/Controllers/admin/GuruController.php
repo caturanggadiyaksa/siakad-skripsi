f5a2b67eea50aa55a43dd92e138b52e39e61e9f3
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-22 13:54:02 +0700

    add crud method untuk route guru di menu admin

--- app/Http/Controllers/admin/GuruController.php
+++ app/Http/Controllers/admin/GuruController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin\Guru;
 
 class GuruController extends Controller
 {
@@ -14,10 +15,55 @@ class GuruController extends Controller
        
         if ($request->user()->role == 'admin') {
             // dd($request->user()->role);
-            return view('dashboard.admin.guru', $data);
+            $guru = Guru::all();
+            return view('dashboard.admin.guru', ['guru' => $guru], $data);
         } else {
             
             return view('dashboard.guru.guru', $data);
         }
+    }
+
+
+    public function tambah() {
+        $data = ['title' => 'Add New Guru'];
+        return view('dashboard.admin.crud.guru.create', $data);
+
+    }
+    public function store(Request $request) {
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        Guru::create([
+            'nama' => $request->nama,
+            'alamat' => $request->alamat
+        ]);
+
+        return redirect('/guru');
+    }
+
+    public function edit($id) {
+        $guru = Guru::find($id);
+        return view('dashboard.admin.crud.guru.edit', ['guru' => $guru]);
+    }
+
+    public function update($id, Request $request) {
+        $this->validate($request, [
+            'nama' => 'required',
+            'alamat' => 'required'
+        ]);
+
+        $guru = Guru::find($id);
+        $guru->nama = $request->nama;
+        $guru->alamat = $request->alamat;
+        $guru->save();
+        return redirect('/guru');
+    }
+
+    public function hapus($id) {
+        $guru = Guru::find($id);
+        $guru->delete();
+        return redirect('/guru');
     }
 }
