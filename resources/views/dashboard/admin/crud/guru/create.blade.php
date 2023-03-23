45eb122f93f2ad9e4b800b7d6d0fac3dd8bf4f43
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-03-23 11:37:17 +0700

    add form add data guru

---
+++ resources/views/dashboard/admin/crud/guru/create.blade.php
@@ -0,0 +1,68 @@
+@extends('dashboard.admin.master')
+ 
+
+@section('konten')
+
+<div class="container-fluid">
+  <div class="card col-12 mt-2 mb-2">
+    <form class="m-4 p-4" action="/guru/store" method="POST">
+      {{ csrf_field() }}
+     
+
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Nip</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Nama</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Jenis Kelamin</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Alamat</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row">
+        <label for="inputPassword" class="col-sm-2 col-form-label">No Telepon</label>
+        <div class="col-sm-10">
+          <input type="password" class="form-control" id="inputPassword" placeholder="">
+        </div>
+      </div>
+      <div class="form-group row" style="justify-content: center;">
+        <div class="col-2">
+          <input type="submit" class="form-control"  value="Simpan">
+        </div>
+        <div class="col-2">
+          <a href="/guru">
+            <input type="button" class="form-control"  value="Cancel">
+          </a>
+        
+        </div>
+      
+      </div>
+   
+    </form>
+  </div>
+  
+
+</div>
+
+@endsection
+
