a6fa8b33537bcd266ae4ba4b1aababcb60fa94e2
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-04 09:36:51 +0700

    add function index controller keuangan

--- app/Http/Controllers/admin/KeuanganController.php
+++ app/Http/Controllers/admin/KeuanganController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class KeuanganController extends Controller
 {
     //
+    public function index() {
+
+        $countSiswa = DB::table('table_siswa')->count();
+        $countGuru = DB::table('table_guru')->count();
+       
+        $data = [
+            'title' => 'Keuangan',
+            'countSiswa' => $countSiswa,
+            'countGuru' => $countGuru,
+        ];
+        return view('dashboard.admin.keuangan', $data);
+        // return view('dashboard.admin.keuangan', $data, $countSiswa, $countGuru);
+    }
 }
