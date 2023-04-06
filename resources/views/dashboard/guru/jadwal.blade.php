faf28bfb925d97a624dcc220ce669a6501a1e09a
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-06 10:28:29 +0700

    add view jadwal dashboard guru

---
+++ resources/views/dashboard/guru/jadwal.blade.php
@@ -0,0 +1,232 @@
+@extends('dashboard.guru.master')
+ 
+@section('content')
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-8 card" style="min-height: 85vh; border-radius: 20px;">
+            <div class="row mt-4">
+                <div class="col-6">
+                    <div>
+                        <h5>Senin</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div>
+                        <h5>Selasa</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-6">
+                    <div>
+                        <h5>Rabu</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div>
+                        <h5>Kamis</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-6">
+                    <div>
+                        <h5>Jumat</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div>
+                        <h5>Sabtu</h5>
+                    </div>
+                    <div class="card" style="height: 74px; border-radius: 20px 0px 0px 20px;">
+                        <div class="d-flex">
+                            <div class="" style="background: #4D44B5; height: 74px; width: 10%; border-radius: 20px 0px 0px 20px;">
+                               
+                            </div>
+                            <div class="d-flex align-items-center" style="background: #C1BBEB; height: 74px; width: 90%;">
+                                <div class="d-flex mx-2 align-items-center">
+                                    <div>
+                                        <h6 style="color:#4D44B5;">Matematika</h6>
+                                    </div>
+                                    <div class="row align-items-center mx-2">
+                                        <div class="col">
+                                            <p style="color: #111213;" class="m-0">March 20, 2023</p>
+                                            <span style="color: #121213;" class="m-0">19.00 - 20.00</span>
+                                        </div>
+                                          
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+        <div class="col-4 card bg-transparent shadow-none" style="min-height: 85vh; border: none;">
+            <div class="container">
+                <div class="card " style=" border-radius: 20px; height: 6rem;">
+                   <div class="col-12 " style="border-radius: 20px; ">
+                        
+                            <div class="row " style="height: 6rem;">
+                                <div class="col d-flex align-items-center">
+                                    <div class="px-2" style="margin: 0;">
+                                        <div class="col mb-2">
+                                            <h6 style="color: #303972;" class="mb-2">Jadwal Ujian</h6>
+                                            <span style="color: #A098AE;" class="m-0">Thursday, 10th April , 2021</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                   </div>
+                   
+                </div> 
+
+                <div class="container mt-4">
+                  
+                    <div class="card mt-4" style="border-radius: 20px; height: 8rem;">
+                        <div class="col-12" style="border-radius: 20px;">
+                            <div class="row">
+                                <div id="warna" class="col-2" style="height: 8rem; border-radius: 20px 0 0 20px; background-color:#4D44B5;"></div>
+                                <div id="jadwal" class="col-10" style="height: 8rem; border-radius: 0 20px 20px 0;">
+                                    <div id="pengumuman" class="d-flex align-items-center">
+                                        <div class=" mt-4" style="">
+                                            <div class="col mb-2">
+                                                <h6 style="color: #303972;" class="m-0">Matematika</h6>
+                                                <span style="color: #A098AE;" class="m-0">XI - A</span>
+                                            </div>
+                                            <div class="col">
+                                                <p style="color: #303972;" class="m-0">March 20, 2023</p>
+                                                <span style="color: #A098AE;" class="m-0">19.00 - 20.00</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    
+                </div>
+            </div>
+        </div>
+   </div>
+
+</div>
+@endsection
