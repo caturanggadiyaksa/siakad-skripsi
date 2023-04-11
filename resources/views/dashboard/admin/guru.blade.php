fb9c333db866404b7952ebe6be1cb8b348d1b0e0
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-11 19:31:08 +0700

    hapus pencarian

--- resources/views/dashboard/admin/guru.blade.php
+++ resources/views/dashboard/admin/guru.blade.php
@@ -6,7 +6,7 @@
 	
 <div class="container-fluid">
     <div class=" d-flex justify-content-between align-items-center">
-        <div class="col-3" style="">
+        {{-- <div class="col-3" style="">
             <div class="input-group" >
               <button style="background-color: #FFFFFF; border-radius: 20px 0 0 20px;" class=" border-0 px-3" type="button">
                 <i style="color:#4D44B5;" class="far fa-search"></i>
@@ -14,9 +14,9 @@
               <input style="background-color: #FFFFFF; border-radius: 0 20px 20px 0;" type="text" class="form-control border-0" placeholder="Cari" aria-label="Cari" aria-describedby="basic-addon2">
             
             </div>
-        </div>
+        </div> --}}
         <div class="d-flex justify-content-end align-items-center">
-            <div class="col-6">
+            {{-- <div class="col-6">
 
                 <div class="d-flex justify-content-center align-items-center" style="width: 150px; height: 40px; border-radius: 40px; padding: 6px 15px; border: 2px solid #4D44B5;">
                     <select name="" id="" style="border: none; background-color: transparent;">
@@ -24,8 +24,8 @@
                         <option value="VII A">VII A</option>
                     </select>
                 </div>
-            </div>
-            <div class="col-6">
+            </div> --}}
+            <div class="col-12">
         
                 <a href="/guru/tambah">
                     <div class="d-flex justify-content-center align-items-center" style="width: 150px; height: 40px; border-radius: 40px; padding: 6px 15px; background: #4D44B5; ">
