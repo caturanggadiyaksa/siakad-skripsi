6c64146f6f44fffd781b7404bf19050d55e0c387
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-11 20:32:19 +0700

    add model biaya siswa

---
+++ app/Models/BiayaSiswa.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BiayaSiswa extends Model
+{
+    use HasFactory;
+    protected $table = "biaya_siswa";
+}
