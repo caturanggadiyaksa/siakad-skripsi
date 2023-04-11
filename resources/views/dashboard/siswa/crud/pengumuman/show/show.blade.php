a94912f29bb64b80782e3d048f4ec264a3ebd399
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-11 20:17:39 +0700

    add view show menu pengumuman dashboard siswa

---
+++ resources/views/dashboard/siswa/crud/pengumuman/show/show.blade.php
@@ -0,0 +1,11 @@
+@extends('dashboard.siswa.master')
+ 
+@section('content-siswa')
+
+<div class="container-fluid">
+    <div class="col-8 card" style="min-height: 85vh; border-radius: 20px;">
+        
+    </div>
+</div>
+ 
+@endsection
