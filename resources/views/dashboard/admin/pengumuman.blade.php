1bc924bafc9084ddfbc8a64a64b136b61d648aad
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-18 20:40:30 +0700

    edit view pengumuman dashboard admin

--- resources/views/dashboard/admin/pengumuman.blade.php
+++ resources/views/dashboard/admin/pengumuman.blade.php
@@ -22,7 +22,8 @@
             <div class="container mt-1">
                 <div class="card mt-4" style=" border-radius: 20px; height: 8rem;">
                    <div class="col-12 " style="border-radius: 20px; ">
-                        <div class="row">
+                    @foreach ($pengumuman as $p)
+                        <div class="row mb-4">
                             <div class="" style="width: 8%; height: 8rem; border-radius: 20px 0 0 20px; background-color:#4D44B5;">
                               
                             </div>
@@ -32,12 +33,12 @@
                                     <div id="pengumuman" class="col-7 d-flex align-items-center">
                                         <div class="px-2" style="margin: 0;">
                                             <div class="col mb-2">
-                                                <h6 style="color: #303972;" class="m-0">Pengumuman Pembayaran</h6>
-                                                <span style="color: #A098AE;" class="m-0">Pembayaran</span>
+                                                <h6 style="color: #303972;" class="m-0">{{ $p->judul_pengumuman }}</h6>
+                                                {{-- <span style="color: #A098AE;" class="m-0">Pembayaran</span> --}}
                                             </div>
                                             <div class="col">
-                                                <p style="color: #303972;" class="m-0">March 20, 2023</p>
-                                                <span style="color: #A098AE;" class="m-0">19.00 - 20.00</span>
+                                                <p style="color: #303972;" class="m-0">{{ $p->tgl_posting }}</p>
+                                                {{-- <span style="color: #A098AE;" class="m-0">19.00 - 20.00</span> --}}
                                             </div>
                                         </div>
                                     </div>
@@ -45,7 +46,7 @@
                                     <div id="btn-pengumuman" class="col-4 d-flex justify-content-center align-items-center" style="height: 8rem;">
                                         <div>
                                             {{-- {{ $p->id }} --}}
-                                            <a href="/pengumuman/show">
+                                            <a href="/pengumuman/show/{{ $p->id }}">
                                                 <div class="d-flex justify-content-center align-items-center" style="width: 100px; height: 40px; border-radius: 40px; padding: 6px 15px; background: #4D44B5;">
                                                     <button style="border: none; background-color: transparent; color: #FFFFFF;" class="align-items-center">
                                                         <span>View</span>
@@ -59,6 +60,7 @@
                                 </div>
                             </div>
                         </div>
+                    @endforeach
                    </div>
                 </div>
                 
