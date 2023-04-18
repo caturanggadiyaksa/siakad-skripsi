820208e0e415292bb72329e5c996de93e70a4903
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-18 20:35:16 +0700

    edit view show pengumuman dashboard siswa

--- resources/views/dashboard/siswa/crud/pengumuman/show/show.blade.php
+++ resources/views/dashboard/siswa/crud/pengumuman/show/show.blade.php
@@ -4,7 +4,22 @@
 
 <div class="container-fluid">
     <div class="col-8 card" style="min-height: 85vh; border-radius: 20px;">
-        
+        <div>
+            <h4>{{ $pengumuman->judul_pengumuman }}</h4>
+        </div>
+
+        <div>
+            <p>{{ $pengumuman->isi_pengumuman }}</p>
+        </div>
+
+        <div>
+            <div class="col-2">
+                <a href="/pengumuman/siswa">
+                  <input type="button" class="form-control"  value="Back">
+                </a>
+              
+              </div>
+        </div>
     </div>
 </div>
  
