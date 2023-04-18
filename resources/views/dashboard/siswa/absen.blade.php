37bc5da78a94bba0ba419677376d9af9c2251b9c
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-04-18 20:38:08 +0700

    add tabel absen

--- resources/views/dashboard/siswa/absen.blade.php
+++ resources/views/dashboard/siswa/absen.blade.php
@@ -5,7 +5,7 @@
     <div class="d-flex justify-content-start align-items-center">
         <div class="row">
 
-            <div class="d-flex justify-content-center align-items-center" style="width: 150px; height: 40px; border-radius: 40px; padding: 6px 15px; border: 2px solid #4D44B5;">
+            {{-- <div class="d-flex justify-content-center align-items-center" style="width: 150px; height: 40px; border-radius: 40px; padding: 6px 15px; border: 2px solid #4D44B5;">
                 <select name="" id="" style="border: none; background-color: transparent;">
                     <option value="">Kelas</option>
                     <option value="VII A">VII A</option>
@@ -19,21 +19,17 @@
                     </button>
                 </a>
                 
-            </div>
+            </div> --}}
         </div>
         
     </div>
 
-    <div class="mt-4">
+    {{-- <div class="mt-4">
         <table id="myTable" class="display">
             <thead>
                 <tr>
                     <th>Nama</th>
                     <th>Kelas</th>
-                    {{-- <th>Jurusan</th>
-                    <th>Tahun Masuk</th> --}}
-                    {{-- <th>Tanggal Lahir</th>
-                    <th>Nomor Telepon</th> --}}
                     <th>Action</th>
                 </tr>
             </thead>
@@ -49,14 +45,14 @@
                         @if(!in_array($a->mapel->nama, $mapelArray))
                             <tr>
                                 <td>{{ $a->mapel->nama }}</td>
-                                {{-- <td> --}}
+                                
                                     @foreach($s->absen->where('mapel.nama', $a->mapel->nama) as $absen)
                                        <td> {{ $absen->keterangan }} </td>
                                     @endforeach
                                     @if($s->absen->where('mapel.nama', $a->mapel->nama)->isEmpty())
                                         <td>-</td>
                                     @endif
-                                {{-- </td> --}}
+                             
                             </tr>
                             @php
                                 $mapelArray[] = $a->mapel->nama;
@@ -65,17 +61,81 @@
                     @endforeach
                 @endforeach
 
-                    {{-- <th style="font-size: 18px !important;">
-                        <a href="/siswa/show/{{ $s->id }}">
-                            <button style="font-size: 12px !important; color:#FFFFFF !important; text-transform: none !important;" class="btn btn-primary">Show</button>
-                        </a>
-                        
-                       
-                      
-                    </th> --}}
-                  
                     
                
+            </tbody>
+        </table>
+    </div> --}}
+    <div class="table mt-4 table-sm" style="text-align:center; color: #4D44B5;">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Mata Pelajaran</th>
+                    <th scope="col">Pertemuan 1</th>
+                    <th scope="col">Pertemuan 2</th>
+                    <th scope="col">Pertemuan 3</th>
+                    <th scope="col">Pertemuan 4</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($siswa as $s)
+                    @php
+                        $mapelArray = [];
+                    @endphp
+
+                    @foreach($s->absen as $a)
+                    @if(!in_array($a->mapel->nama, $mapelArray))
+                        <tr>
+                        <th style="color: #587bcb;">{{ $a->mapel->nama }}</th>
+                        @for($i = 1; $i <= 4; $i++)
+                            @php
+                            $absenPertemuan = $s->absen->where('mapel.nama', $a->mapel->nama)->where('pertemuan', $i)->first();
+                            @endphp
+                            <td>{{ $absenPertemuan ? $absenPertemuan->keterangan : 'Belum' }}</td>
+                        @endfor
+                        <td>
+                            <a href="/absen/show/{{ $a->mapel->id }}">
+                            <button type="button" class="btn btn-primary btn-sm" style="border-radius: 15px; width: 76px; font-size: 12px; height: 35px; font-weight: bold; color: white">Show</button>
+                            </a>
+                        </td>
+                        </tr>
+                        @php
+                        $mapelArray[] = $a->mapel->nama;
+                        @endphp
+                    @endif
+                    @endforeach
+
+                    {{-- @foreach($s->absen as $a)
+                        @if(!in_array($a->mapel->nama, $mapelArray))
+                            <tr>
+                                <th style="color: #587bcb;">{{ $a->mapel->nama }}</th>
+                                
+                                    @foreach($s->absen->where('mapel.nama', $a->mapel->nama) as $absen)
+                                       <td> {{ $absen->keterangan }} </td>
+                                    @endforeach
+                                    @if($s->absen->where('mapel.nama', $a->mapel->nama)->isEmpty())
+                                        <td>-</td>
+                                    @endif
+                                
+                                <td>
+                                    <a href="/keuangan/edit/">
+                                        <button type="button" class="btn btn-primary btn-sm" style="border-radius: 15px; width: 76px; font-size: 12px; height: 35px; font-weight: bold; color: white">Show</button>
+                                    </a>
+                                   
+                                    
+                                </td>
+                            </tr>
+                            @php
+                                $mapelArray[] = $a->mapel->nama;
+                            @endphp
+                        @endif
+                    @endforeach --}}
+                @endforeach
+               
+               
+
+
             </tbody>
         </table>
     </div>
