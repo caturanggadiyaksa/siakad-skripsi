17f278e57715ae8778c1fe0b0b9fc4c92e777a1c
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-05-07 09:09:05 +0700

    create model tugas

---
+++ app/Models/Tugas.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tugas extends Model
+{
+    use HasFactory;
+
+    protected $table = "table_tugas";
+
+    protected $fillable = [
+        'id',
+        'judul',
+        'status',
+        
+    ];
+}
