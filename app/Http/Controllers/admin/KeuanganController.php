9e516e7df678bef2e37ea3ee0b23b9a61928bb84
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-05-07 09:13:42 +0700

    edit keuangan controller

--- app/Http/Controllers/admin/KeuanganController.php
+++ app/Http/Controllers/admin/KeuanganController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Biayasekolah;
+use App\Models\BiayaSiswa;
+use App\Models\Siswa;
 class KeuanganController extends Controller
 {
     //
@@ -13,13 +15,51 @@ class KeuanganController extends Controller
 
         $countSiswa = DB::table('table_siswa')->count();
         $countGuru = DB::table('table_guru')->count();
-       
+        $biayasekolah = Biayasekolah::all();
+        $biayaSiswa = Biayasiswa::all();
+        $siswa = Siswa::with('biayasiswa')->get();
         $data = [
             'title' => 'Keuangan',
             'countSiswa' => $countSiswa,
             'countGuru' => $countGuru,
+            'biayasekolah' => $biayasekolah,
+            'biayaSiswa' => $biayaSiswa,
+            'siswa' => $siswa,
         ];
         return view('dashboard.admin.keuangan', $data);
-        // return view('dashboard.admin.keuangan', $data, $countSiswa, $countGuru);
+      
+    }
+
+    public function storeSekolah(Request $request) {
+        Biayasekolah::create([
+            'invoice' => $request->invoice,
+            'tanggal_pengeluaran' => $request->tanggal_pengeluaran,
+            'jenis_biaya' => $request->jenis_biaya,
+            'nominal' => $request->nominal
+        ]);
+
+        return redirect('/keuangan');
+    }
+
+    public function tambahSekolah() {
+        $data = ['title' => 'Add New Biaya Pengeluaran Sekolah'];
+        return view('dashboard.admin.crud.keuangan.create', $data);
+    }
+
+    public function storeSiswa(Request $request) {
+        BiayaSiswa::create([
+            'siswa_id' => $request->siswa_id,
+            'jenis_biaya' => $request->jenis_biaya,
+            'nominal' => $request->nominal,
+            'tanggal_pembayaran' => $request->tanggal_pembayaran
+           
+        ]);
+
+        return redirect('/keuangan');
+    }
+
+    public function tambahSiswa() {
+        $data = ['title' => 'Add New Biaya Siswa'];
+        return view('dashboard.admin.crud.keuangan.create-siswa', $data);
     }
 }
