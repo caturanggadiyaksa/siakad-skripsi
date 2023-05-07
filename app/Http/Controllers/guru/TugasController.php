61a5539ddc01fe45d200315ade48ed376620db81
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-05-07 09:10:58 +0700

    edit tugas controller

--- app/Http/Controllers/guru/TugasController.php
+++ app/Http/Controllers/guru/TugasController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\guru;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Siswa;
+use App\Models\Mapel;
+use App\Models\Tugas;
 
 class TugasController extends Controller
 {
@@ -17,7 +19,49 @@ class TugasController extends Controller
         if ($request->user()->role == 'guru') {
             $siswa = Siswa::all();
             return view('dashboard.guru.tugas', $data,  ['siswa' => $siswa]);
-        } 
+        } elseif ($request->user()->role == 'siswa') {
+            $mapel = Mapel::all();
+            $tugas = Tugas::all();
+            $dataTugas = [
+                'mapel' => $mapel,
+                'tugas' => $tugas
+            ];
+            return view('dashboard.siswa.tugas', $data, $dataTugas);
+            
+        } else {
+            // return redirect('/login');
+        }
 
     }
+
+    public function requestMapel(Request $request, $mapel)
+    {
+        $data = ['title' => 'Tugas'];
+        $mapelQuery = $mapel;
+        $mapelData = Mapel::all();
+       
+        $result = Tugas::where('nama_mapel', $mapelQuery)->get();
+        $dataTugas = [
+            'mapelSelected' => $mapel,
+            'mapel' => $mapelData,
+            'tugasid' => $result
+        ];
+        // dd($result);
+    
+        return view('dashboard.siswa.tugasid', $data, $dataTugas);
+    }
+    public function tambahTugas($mapel) {
+        $data = ['title' => 'Submit Tugas'];
+        $mapelQuery = $mapel;
+        $mapelData = Mapel::all();
+       
+        $result = Tugas::where('nama_mapel', $mapelQuery)->get();
+        $dataTugas = [
+            'mapelSelected' => $mapel,
+            'mapel' => $mapelData,
+            'tugasid' => $result
+        ];
+        return view('dashboard.siswa.addtugas', $data, $dataTugas);
+    }
+    
 }
