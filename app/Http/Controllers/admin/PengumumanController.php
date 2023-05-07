af34a6157e6717ae7bf36a9a22f5ce29577e8f29
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-05-07 09:12:51 +0700

    edit pengumuman controler

--- app/Http/Controllers/admin/PengumumanController.php
+++ app/Http/Controllers/admin/PengumumanController.php
@@ -11,19 +11,9 @@ class PengumumanController extends Controller
     //
     public function index(Request $request){
         $data = ['title' => 'Pengumuman'];
-        
-        if ($request->user()->role == 'admin') {
-           
-            return view('dashboard.admin.pengumuman', $data);
+        $pengumuman = Pengumuman::all();
 
-        } elseif ($request->user()->role == 'siswa') {
-            $pengumuman = Pengumuman::all();
-            return view('dashboard.siswa.pengumuman', $data, ['pengumuman' => $pengumuman]);
-            
-        }else {
-            
-            return view('/login');
-        }
+        return view('dashboard.admin.pengumuman', $data, ['pengumuman' => $pengumuman]);
     }
 
     public function tambah() {
@@ -33,19 +23,42 @@ class PengumumanController extends Controller
     }
     public function store(Request $request) {
         $this->validate($request, [
-            'nama' => 'required',
-            'alamat' => 'required'
+            'judul_pengumuman' => 'required',
+            'isi_pengumuman' => 'required'
         ]);
 
-        Siswa::create([
-            'nama' => $request->nama,
-            'id_kelas' => $request->id_kelas,
-            'jenis_kelamin' => $request->jenis_kelamin,
-            'alamat' => $request->alamat,
-            'tanggal_lahir' => $request->tanggal_lahir,
-            'nomor_telepon' => $request->no_telepon
+        Pengumuman::create([
+            'judul_pengumuman' => $request->judul_pengumuman,
+            'isi_pengumuman' => $request->isi_pengumuman,
+            'tgl_posting' => $request->tanggal
         ]);
 
+        return redirect('/pengumuman');
+    }
+
+
+    public function show($id) {
+        $data = ['title' => 'Show Data Pengumuman'];
+        $pengumuman = Pengumuman::find($id);
+
+        
+        return view('dashboard.admin.crud.pengumuman.show', ['pengumuman' => $pengumuman], $data);
+    }
+
+    public function delete($id) {
+        $pengumuman = Pengumuman::find($id);
+        $pengumuman->delete();
+        // dd($pengumuman);
+        return redirect('/pengumuman');
+    }
+
+    public function update($id, Request $request) {
+
+        $pengumuman = Pengumuman::find($id);
+        $pengumuman->judul_pengumuman = $request->judul_pengumuman;
+        $pengumuman->isi_pengumuman = $request->isi_pengumuman;
+        $pengumuman->tgl_posting = $request->tanggal;
+        $pengumuman->save();
         return redirect('/pengumuman');
     }
 }
