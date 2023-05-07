7404b122895511f76858cd9d7d5de82c02ac5778
caturanggadiyaksa <adiyaksacaturangga@gmail.com>
2023-05-07 09:12:01 +0700

    add nilai controller

--- app/Http/Controllers/guru/NilaiController.php
+++ app/Http/Controllers/guru/NilaiController.php
@@ -17,7 +17,13 @@ class NilaiController extends Controller
         if ($request->user()->role == 'guru') {
             $siswa = Siswa::all();
             return view('dashboard.guru.nilai', $data,  ['siswa' => $siswa]);
-        } 
+        } elseif ($request->user()->role == 'siswa') {
+            
+            return view('dashboard.siswa.nilai', $data );
+
+        } else {
+            // return redirect('/login');
+        }
 
     }
 }
